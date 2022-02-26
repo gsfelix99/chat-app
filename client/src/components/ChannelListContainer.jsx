2a61de91789d69b9dee46c1cb1cf966026051f7b
Gabriel Felix <g.felix@estudante.ifmt.edu.br>
2022-02-25 23:57:58 -0400

    Adicionado componente TeamChannelPreview.jsx no arquivo index.js e ao ChannelListContainer.jsx.

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -44,7 +44,29 @@ const ChannelListContainer = () => {
                             type="team"
                         />
                     )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            { ...previewProps }
+                            type="team"
+                        />
+                    )}
+                />
 
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => {}}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            { ...listProps }
+                            type="messaging"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            { ...previewProps }
+                            type="messaging"
+                        />
+                    )}
                 />
             </div>
         </>
